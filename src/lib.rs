ab22ca3381ddf444cf51340a8658ac50328b930f
Павел <pashanoy@gmail.com>
2025-12-09 16:40:03 +0200

    Update the project to be usable as a crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,103 +1,14 @@
-pub mod async_runtime;
+/// An adapter library to run Tokio runtime on the main thread of
+/// Godot runtime.
+///
+/// Using this should remove the pain point of needing to avoid
+/// all Godot API in rust-async contexts. Also allows awaiting
+/// rust futures, using `Signal`s as adapters between rust and
+/// godot worlds.
+///
+/// There is only one limitation of this library - you must never
+/// create a future while there is no `SceneTree` available, as it
+/// is used to drive spawned futures, during `process_frame` signal.
+mod st_tokio;
 
-use godot::prelude::*;
-use godot::classes::{Engine, ISprite2D, Sprite2D};
-
-use crate::async_runtime::TokioRuntime;
-
-#[derive(GodotClass)]
-#[class(base=Sprite2D)]
-struct Player {
-    //speed: f64,
-    angular_speed: f64,
-
-    #[var]
-    time_elapsed: f64,
-
-    base: Base<Sprite2D>
-}
-
-#[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-        
-        Self {
-            //speed: 400.0,
-            time_elapsed: 0.0,
-            angular_speed: std::f64::consts::PI,
-            base,
-        }
-    }
-
-    fn physics_process(&mut self, delta: f64) {
-        // In GDScript, this would be: 
-        // rotation += angular_speed * delta
-        
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
-        // The 'rotate' method requires a f32, 
-        // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
-    }
-}
-
-#[godot_api]
-impl Player {
-    #[signal]
-    fn my_signal(thing: i32);
-
-    #[func]
-    fn do_the_thing(&mut self) -> Signal {
-        // Needs to become a pointer to be movable. `to_gd` warning apply
-        let self_gd = self.to_gd();
-
-        // `spawn_signal` returns a `Signal`, that can be awaited in Godot
-        let signal = TokioRuntime::spawn_signal(async move {
-            // Tokio can handle awaiting for signals
-            // Also, we can access Godot values, like `Gd` pointers
-            // as `Send` bound is not required for these futures
-            let _thing = self_gd.signals().my_signal().to_future().await;
-
-            reqwest::get("https://example.com")
-                .await
-                .unwrap()
-                .text()
-                .await
-                .unwrap()
-        });
-
-        let signal2 = TokioRuntime::spawn_signal(async move {
-            // A Godot-native signal can be `await`ed as well, but the type information
-            // will not be available, so you have to provide the types manually
-            let (res,): (GString,) = signal.to_future().await;
-            // The future runs on the same thread as the `spawn` call,
-            // so we can access binds like `godot_print`
-            godot_print!("{res}");
-
-            "signal2 finished"
-        });
-
-        signal2
-    }
-}
-
-struct MyExtension;
-
-#[gdextension]
-unsafe impl ExtensionLibrary for MyExtension {
-    fn on_level_deinit(level: InitLevel) {
-        match level {
-            InitLevel::Scene => {
-                let mut engine = Engine::singleton();
-
-                // Here is where we free our async runtime singleton from memory.
-                if let Some(async_singleton) = engine.get_singleton(TokioRuntime::SINGLETON) {
-                    engine.unregister_singleton(TokioRuntime::SINGLETON);
-                    async_singleton.free();
-                }
-            }
-            _ => (),
-        }
-    }
-}
-
+pub use st_tokio::TokioRuntime;
--- src/st_tokio.rs
+++ src/st_tokio.rs
@@ -1,5 +1,4 @@
-
-use std::rc::Rc;
+use std::sync::{LazyLock, Mutex};
 
 use godot::{classes::Engine, prelude::*};
 use tokio::{
@@ -7,27 +6,38 @@ use tokio::{
     task::{JoinHandle, LocalSet},
 };
 
+static TOKIO_BUILDER: LazyLock<Mutex<runtime::Builder>> = LazyLock::new(|| {
+    let mut builder = runtime::Builder::new_current_thread();
+    builder.enable_all();
+
+    Mutex::new(builder)
+});
+
 /// Holds the actual tokio [`Runtime`] that drives the spawned futures,
 /// and a [`LocalSet`] which holds said futures to run on the current/main thread
 #[derive(GodotClass)]
 #[class(base=Object)]
 pub struct TokioRuntime {
     base: Base<Object>,
-    runtime: Rc<Runtime>,
+    runtime: Runtime,
     local_set: LocalSet,
 }
 
 #[godot_api]
 impl IObject for TokioRuntime {
     fn init(base: Base<Object>) -> Self {
-        let runtime = runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
+        let mut builder_guard = TOKIO_BUILDER
+            .lock()
+            .expect("Failed getting TOKIO_BUILDER static during TokioRuntime::init");
+
+        // Get the builder in the static by replacing it with the one in the Mutex
+        let mut new_builder = runtime::Builder::new_current_thread();
+        new_builder.enable_all();
+        let mut builder = std::mem::replace(&mut *builder_guard, new_builder);
 
         Self {
             base,
-            runtime: Rc::new(runtime),
+            runtime: builder.build().unwrap(),
             local_set: Default::default(),
         }
     }
@@ -38,66 +48,81 @@ impl TokioRuntime {
     pub const SINGLETON: &'static str = "Tokio";
 
     /// The only right way to create a [`TokioRuntime`]
-    /// 
+    ///
     /// Creates the runtime and starts the [`tick`][Self::tick]ing function,
     /// that drives spawned futures
-    /// 
+    ///
     /// Authors note: there might be a better way to wire into the idle time
     /// of the engine, I didn't find it personally. Also I have no idea what
-    /// happens if idle time is starved, but if it is I imagine you have bigger
+    /// happens if idle time is starved, but if it is, I imagine you have bigger
     /// problems anyway
-    fn init_and_start() -> Gd<Self> {
-        let mut async_runtime = Self::new_alloc();
+    fn init_and_start() -> Option<Gd<Self>> {
+        let async_runtime = Self::new_alloc();
         Engine::singleton()
-            .get_main_loop()
-            .unwrap()
+            .get_main_loop()?
             .cast::<SceneTree>()
             .signals()
             .process_frame()
-            .connect_other(
-                &mut async_runtime,
-                TokioRuntime::tick
-            );
+            .connect_other(&async_runtime, TokioRuntime::tick);
 
-        async_runtime
+        Some(async_runtime)
+    }
+
+    /// Replaces a runtime builder that will be used to build a Tokio runtime.
+    /// Builder is not clone-able, so if you unregister the runtime, you need
+    /// to re-set the builder before spawning any futures, if you need custom
+    /// configuration.
+    ///
+    /// If this function isn't called - a runtime is initialized with
+    /// `Builder::new_current_thread().enable_all()` on first spawn of a future.
+    ///
+    /// # Warning
+    ///
+    /// It is possible to pass a multithreaded builder here, but LocalSet is
+    /// the storage of the futures, so they still will run on one thread.
+    pub fn set_builder(tokio_builder: runtime::Builder) {
+        let mut builder_guard = TOKIO_BUILDER
+            .lock()
+            .expect("Failed getting TOKIO_BUILDER static during set_builder");
+
+        *builder_guard = tokio_builder;
     }
 
     /// Get an active singleton, create one if it doesn't exist.
-    /// 
+    ///
     /// #### WARNING! Cannot be used during level_init
-    /// 
+    ///
     /// Expects for a main loop to be initialized, which means
     /// it's usually initialized on the first call to [`spawn`][Self::spawn]
     /// or [`spawn_signal`][Self::spawn_signal]
-    /// 
+    ///
     /// Authors note: emits an error on gdextension load. Again, there might
     /// be a better way to do this over a singleton, but I'm not sure what
     /// way is
-    pub fn singleton() -> Gd<TokioRuntime> {
+    pub fn singleton() -> Option<Gd<TokioRuntime>> {
         match Engine::singleton().get_singleton(Self::SINGLETON) {
-            Some(singleton) => singleton.cast::<Self>(),
+            Some(singleton) => Some(singleton.cast::<Self>()),
             None => {
-                let singleton = TokioRuntime::init_and_start();
-                Engine::singleton()
-                    .register_singleton(TokioRuntime::SINGLETON, &singleton);
+                let singleton = TokioRuntime::init_and_start()?;
+                Engine::singleton().register_singleton(TokioRuntime::SINGLETON, &singleton);
 
-                singleton
-            },
+                Some(singleton)
+            }
         }
     }
 
     /// #### WARNING! [`Spawn`][Self::spawn] warnings apply
-    /// 
+    ///
     /// Spawns a provided future on the runtime, and returns a [`Signal`]
     /// that can be `await`ed on the Godot side (and on Rust side, with
     /// [`.to_future`][Signal::to_future]).
-    /// 
+    ///
     /// The resulting [`Signal`] is type-erased, because [`TypedSignal`][godot::register::TypedSignal]
     /// doesn't have public API constructor, nor does it work with godot FFI
-    /// 
+    ///
     /// Since the runtime is running on the current thread - you can
     /// move `!Send` values in, like [`Gd`] pointers
-    /// 
+    ///
     /// The signal does not need to be `await`ed to finish
     pub fn spawn_signal<F, R>(future: F) -> Signal
     where
@@ -111,10 +136,11 @@ impl TokioRuntime {
         Self::spawn(async move {
             let result = future.await;
 
-            let callable = Callable::from_fn("TokioRuntime::spawn_signal::callable", move |_args| {
-                signal_holder.emit_signal("finished", &[result.to_variant()]);
-                Variant::nil()
-            });
+            let callable =
+                Callable::from_fn("TokioRuntime::spawn_signal::callable", move |_args| {
+                    signal_holder.emit_signal("finished", &[result.to_variant()]);
+                    Variant::nil()
+                });
 
             callable.call_deferred(&[]);
         });
@@ -128,24 +154,40 @@ impl TokioRuntime {
     /// <br><br>
     ///   
     /// A wrapper function for the [`tokio::spawn`] function.
-    /// 
+    ///
     /// Can be called both in async and non-async contexts.
-    /// 
+    ///
     /// Allows `!Send` futures
-    pub fn spawn<F>(future: F) -> tokio::task::JoinHandle<F::Output>
+    pub fn spawn<F>(future: F) -> JoinHandle<F::Output>
     where
         F: Future + 'static,
     {
-        let runtime = Self::singleton();
-        
+        let runtime = Self::singleton()
+            .expect("TokioRuntime singleton did not exist while trying to spawn a future");
+
         runtime.bind().local_set.spawn_local(future)
     }
 
+    /// A spawn function that runs a future on a runtime directly.
+    /// Might be useful, if you are deciding to use a multithreaded runtime,
+    /// and know what you are doing. I won't describe all dangers of using
+    /// this.
+    pub fn spawn_rt<F>(future: F) -> JoinHandle<F::Output>
+    where
+        F: Future + Send + 'static,
+        F::Output: Send,
+    {
+        let runtime = Self::singleton()
+            .expect("TokioRuntime singleton did not exist while trying to spawn a future");
+
+        runtime.bind().runtime.spawn(future)
+    }
+
     /// A wrapper function for the [`tokio::spawn_blocking`] function.
-    /// 
+    ///
     /// Use only for blocking operations, like sync HTTP requests,
     /// non-async fs operations, etc.
-    /// 
+    ///
     /// Since the function is running on a separate thread - `Sync`
     /// requirement cannot be relaxed, and `godot_rust` bindings
     /// [limitations][https://github.com/godot-rust/gdext/issues/18] apply
@@ -154,24 +196,38 @@ impl TokioRuntime {
         F: FnOnce() -> R + Send + 'static,
         R: Send + 'static,
     {
-        Self::singleton().bind().runtime.spawn_blocking(func)
+        Self::singleton()
+            .expect("TokioRuntime singleton did not exist while trying to spawn a blocking thread")
+            .bind()
+            .runtime
+            .spawn_blocking(func)
     }
 
     /// Performs an evaluation of currently registered tasks/futures
     /// during idle time on the main thread.
-    /// 
+    ///
     /// This is the main mechanism of driving futures.
     fn tick(&mut self) {
         // Runs tokio runtime...
-        self.runtime.block_on(
-            // ...with the current-thread local_set...
-            self.local_set.run_until(
-                async {
+        self.runtime
+            .block_on(
+                // ...with the current-thread local_set...
+                self.local_set.run_until(async {
                     // ...with a no-op payload. This allows stored futures to
                     // make progress without blocking the thread
                     tokio::task::spawn_local(async {}).await
-                }
+                }),
             )
-        ).unwrap();
+            .unwrap();
+    }
+
+    pub fn unregister_singleton() {
+        let mut engine = Engine::singleton();
+
+        // Here is where we free our async runtime singleton from memory.
+        if let Some(async_singleton) = engine.get_singleton(TokioRuntime::SINGLETON) {
+            engine.unregister_singleton(TokioRuntime::SINGLETON);
+            async_singleton.free();
+        }
     }
 }
